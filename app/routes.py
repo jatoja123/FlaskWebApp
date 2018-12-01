4a40ba69d8b155db4ac9b0f5de083aedf333a41f
n/a <n/a>
2018-12-01 16:49:50 +0100

    dodano /galery i karuzele

--- app/routes.py
+++ app/routes.py
@@ -6,6 +6,10 @@ from flask import render_template
 def index():
     return render_template('index.html', title = "Home page")
 
+@app.route('/galeria')
+def galeria():
+    return render_template('galeria.html')
+
 @app.route('/user/<name>')
 def getUser(name):
     return render_template('user.html', name=name)
